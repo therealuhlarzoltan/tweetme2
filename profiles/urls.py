ef3a87146c77e0b9e98ce4ded9fe08a079562fa1
therealuhlarzoltan <therealuhlarzoltan@gmail.com>
2022-05-06 10:38:41 +0200

    88 - Save 2 Models in 1 Form and 1 View

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import profile_detail_view
+from .views import profile_detail_view, profile_update_view
 
 urlpatterns = [
     path('<str:username>', profile_detail_view),
+    path('edit/', profile_update_view)
 ]
 
